600e7baf2a8ab4e36821b6ce6d82f54b6511233a
gn5 <gn5@sanger.ac.uk>
2020-04-05 15:34:44 +0000

    test

--- .github/run_develop_tests.sh
+++ .github/run_develop_tests.sh
@@ -24,23 +24,23 @@ sudo apt-get update && sudo apt-get install -y \
     pkg-config \
     git \
     cryptsetup
-export VERSION=1.13 OS=linux ARCH=amd64
-wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz
-sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz
-rm go$VERSION.$OS-$ARCH.tar.gz
-export PATH=/usr/local/go/bin:$PATH
-export VERSION=3.5.3
-wget https://github.com/sylabs/singularity/releases/download/v${VERSION}/singularity-${VERSION}.tar.gz && \
-tar -xzf singularity-${VERSION}.tar.gz && \
-cd singularity
-./mconfig
-make -C builddir
-sudo make -C builddir install
-cd ..
-singularity version >> artifacts/test_artifact.log 
+#export VERSION=1.13 OS=linux ARCH=amd64
+#wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz
+#sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz
+#rm go$VERSION.$OS-$ARCH.tar.gz
+#export PATH=/usr/local/go/bin:$PATH
+#export VERSION=3.5.3
+#wget https://github.com/sylabs/singularity/releases/download/v${VERSION}/singularity-${VERSION}.tar.gz && \
+#tar -xzf singularity-${VERSION}.tar.gz && \
+#cd singularity
+#./mconfig
+#make -C builddir
+#sudo make -C builddir install
+#cd ..
+#singularity version >> artifacts/test_artifact.log 
 
 # build singularity images
-bash scripts/build_singularity_containers.sh
+#bash scripts/build_singularity_containers.sh
 
 # Run the tests
 echo test
