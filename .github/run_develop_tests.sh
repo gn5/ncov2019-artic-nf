5932e7e5cf21d5c330f7a82f8fa9e0bafcc7a2b4
gn5 <gn5@sanger.ac.uk>
2020-04-05 15:04:30 +0000

    test

--- .github/run_develop_tests.sh
+++ .github/run_develop_tests.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# set up github Action artifacts
+mkdir -p artifacts
+echo run script run_develop_tests.sh >> artifacts/test_artifact.log
 # Run the tests
 echo test || exit 1
 # Everything passed, exit cleanly.
