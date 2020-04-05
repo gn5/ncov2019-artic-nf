048b93d8bf35fb302b055bb4dc033d01dce51486
gn5 <gn5@sanger.ac.uk>
2020-04-05 18:45:19 +0000

    test

--- .github/run_develop_tests.sh
+++ .github/run_develop_tests.sh
@@ -9,20 +9,23 @@ echo run script run_develop_tests.sh >> artifacts/test_artifact.log
 echo run script .github/install_nextflow_singularity.sh >> artifacts/test_artifact.log
 source .github/install_nextflow_singularity.sh
 
+# install Conda envs
+source .github/install_conda_envs.sh
+
 # build singularity images
-apt-get install uidmap
-sed -i s'/sudo singularity build /singularity build --fakeroot /'g \
-    scripts/build_singularity_containers.sh
-echo run scripts/build_singularity_containers.sh >> artifacts/test_artifact.log
-bash scripts/build_singularity_containers.sh
+#apt-get install uidmap
+#sed -i s'/sudo singularity build /singularity build --fakeroot /'g \
+#    scripts/build_singularity_containers.sh
+#echo run scripts/build_singularity_containers.sh >> artifacts/test_artifact.log
+#bash scripts/build_singularity_containers.sh
 
 # Run the tests
-echo test
-NXF_VER=20.03.0-edge nextflow run main.nf \
-       -profile singularity \
-       --directory /home/ubuntu/fastq \
-       --illumina \
-       --prefix test
+echo run tests
+#NXF_VER=20.03.0-edge nextflow run main.nf \
+#       -profile singularity \
+#       --directory /home/ubuntu/fastq \
+#       --illumina \
+#       --prefix test
 
 # Everything passed, exit cleanly.
 exit 0
