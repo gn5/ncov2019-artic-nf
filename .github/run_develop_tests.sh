e176d4ddab2c4adb0f424315c36d1251b108d971
gn5 <gn5@sanger.ac.uk>
2020-04-05 17:39:56 +0000

    test

--- .github/run_develop_tests.sh
+++ .github/run_develop_tests.sh
@@ -1,60 +1,16 @@
 #!/bin/bash
 
-# write log to github Action artifacts
+# write log as github Action artifact
 mkdir artifacts
 echo run script run_develop_tests.sh >> artifacts/test_artifact.log
 
-# install Conda
-export PATH=/opt/conda/bin:$PATH
-apt-get update --fix-missing && apt-get install -y wget bzip2 ca-certificates \
-    libglib2.0-0 libxext6 libsm6 libxrender1 \
-    git mercurial subversion
-wget --quiet https://repo.anaconda.com/miniconda/Miniconda2-4.5.11-Linux-x86_64.sh -O ~/miniconda.sh && \
-    /bin/bash ~/miniconda.sh -b -p /opt/conda && \
-    rm ~/miniconda.sh && \
-    ln -s /opt/conda/etc/profile.d/conda.sh /etc/profile.d/conda.sh && \
-    echo ". /opt/conda/etc/profile.d/conda.sh" >> ~/.bashrc && \
-    echo "conda activate base" >> ~/.bashrc
+# install Nextflow and Singularity
+echo run script .github/install_nextflow_singularity.sh >> artifacts/test_artifact.log
+source .github/install_nextflow_singularity.sh
 
-# install Nextflow via Conda
-conda install -c bioconda nextflow
-NXF_VER=20.03.0-edge nextflow -version >> artifacts/test_artifact.log
-
-# install Singularity dependencies 
-apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install -y \
-    build-essential \
-    libssl-dev \
-    uuid-dev \
-    libgpgme11-dev \
-    squashfs-tools \
-    libseccomp-dev \
-    wget \
-    pkg-config \
-    git \
-    cryptsetup
-
-# install Go compiler
-rm -r /usr/local/go
-export VERSION=1.13 OS=linux ARCH=amd64
-wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz
-tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz
-rm go$VERSION.$OS-$ARCH.tar.gz
-export PATH=/usr/local/go/bin:$PATH
-
-# install Singularity
-export VERSION=3.5.3
-wget https://github.com/sylabs/singularity/releases/download/v${VERSION}/singularity-${VERSION}.tar.gz && \
-tar -xzf singularity-${VERSION}.tar.gz && \
-cd singularity
-./mconfig
-make -C builddir
-make -C builddir install
-cd ..
-singularity version >> artifacts/test_artifact.log 
-#
 # build singularity images
-#bash scripts/build_singularity_containers.sh
+echo run scripts/build_singularity_containers.sh >> artifacts/test_artifact.log
+bash scripts/build_singularity_containers.sh
 
 # Run the tests
 echo test
