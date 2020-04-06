5b16d40d18de3617522a57a0c609de4e176910a0
gn5 <gn5@sanger.ac.uk>
2020-04-06 10:15:08 +0000

    test

--- .github/install_singularity.sh
+++ .github/install_singularity.sh
@@ -18,7 +18,7 @@ DEBIAN_FRONTEND=noninteractive sudo apt-get install -y \
 sudo rm -r /usr/local/go
 export VERSION=1.13 OS=linux ARCH=amd64
 wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz
-tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz
+sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz
 rm go$VERSION.$OS-$ARCH.tar.gz
 export PATH=/usr/local/go/bin:$PATH
 
