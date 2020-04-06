fc6983c0457751ee3317f9f0a9fad68347b54190
gn5 <gn5@sanger.ac.uk>
2020-04-06 12:31:58 +0000

    test

--- .github/run_develop_tests.sh
+++ .github/run_develop_tests.sh
@@ -3,23 +3,14 @@ set -eo pipefail
 
 # write log as github Action artifact
 mkdir artifacts
-echo run script run_develop_tests.sh >> artifacts/test_artifact.log
+echo run tests >> artifacts/test_artifact.log
 
-# install Conda
-#bash .github/install_conda.sh
-
-# install Nextflow via Conda
-#export PATH=/opt/conda/bin:$PATH
-#conda install -c bioconda nextflow
-
-# test singularity profile
-#sed -i s'/sudo //'g .github/install_singularity.sh
-#bash .github/install_singularity.sh
-#echo run tests >> artifacts/test_artifact.log
 export PATH=/opt/conda/bin:$PATH
-NXF_VER=20.03.0-edge nextflow run main.nf \
+ls -ltra
+singularity --version
+NXF_VER=20.03.0-edge nextflow run ./main.nf \
        -profile singularity \
-       --directory test_data/fastq \
+       --directory ./test_data/fastq \
        --illumina \
        --prefix test
 
