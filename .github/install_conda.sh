371bc8f095f35900097b3fc747dee37fc13182d8
gn5 <gn5@sanger.ac.uk>
2020-04-06 10:01:33 +0000

    test

--- .github/install_conda.sh
+++ .github/install_conda.sh
@@ -2,7 +2,7 @@
 
 # install Conda
 export PATH=/opt/conda/bin:$PATH
-apt-get update --fix-missing && apt-get install -y wget bzip2 ca-certificates \
+sudo apt-get update --fix-missing && sudo apt-get install -y wget bzip2 ca-certificates \
     libglib2.0-0 libxext6 libsm6 libxrender1 \
     git mercurial subversion
 wget --quiet https://repo.anaconda.com/miniconda/Miniconda2-4.5.11-Linux-x86_64.sh -O ~/miniconda.sh && \
